2a49ca97cf98330a00890563a853c15dbeac1173
Stefan Corneliu Petrea <stefan@garage-coding.com>
2018-04-30 18:09:55 +0300

    update

--- t/config-setting-persistence-index.sh
+++ t/config-setting-persistence-index.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-rm ~/postgres.dmp
-/usr/local/pgsql/bin/pg_ctl stop -D /var/lib/postgresql/
-/usr/local/pgsql/bin/pg_ctl start -D /var/lib/postgresql/ -l /var/log/postgresql/logfile
+# We create two databases, p1 and p2.
+# We provision the first database p1 with the sql script config-setting-persistence-index.sql.
+# In order to reproduce, we then dump db p1 into /home/postgres/p1.dmp
+# We then restore p2 from p1.dmp
+# The aim is to see what kinds of log messages we find inside /var/log/postgresql/logfile, specifically
+# the ones produced by pg_restore when it reaches the CREATE INDEX statement
+# that is contained inside p1.dmp
+# 
 
-/usr/local/pgsql/bin/pg_restore -v --no-acl --no-owner -d p1 ~/postgres.dmp
-docker exec -u postgres -t -i pg-9.6.8 /usr/local/pgsql/bin/psql
+rm /home/postgres/p1.dmp
+
+/usr/local/pgsql/bin/dropdb p1
+/usr/local/pgsql/bin/dropdb p2
+
+/usr/local/pgsql/bin/createdb p1
+/usr/local/pgsql/bin/createdb p2
+
+/usr/local/pgsql/bin/psql -d p1 -f /home/postgres/t/config-setting-persistence-index.sql
+/usr/local/pgsql/bin/pg_dump -Fc -d p1 -f /home/postgres/p1.dmp
+
+PGAPPNAME="restore_start" /usr/local/pgsql/bin/psql -d p2 -c 'SELECT 1;'
+PGAPPNAME="restore_run" /usr/local/pgsql/bin/pg_restore -v --no-acl --no-owner -d p2 ~/p2.dmp
+PGAPPNAME="restore_end" /usr/local/pgsql/bin/psql -d p2 -c 'SELECT 1;'
+
