a5e528dad117bf3f15ef39322f671ce25e8931a1
Stefan Corneliu Petrea <stefan@garage-coding.com>
2018-04-30 16:31:04 +0300

    update

--- t/config-setting-persistence-index.sql
+++ t/config-setting-persistence-index.sql
@@ -1,12 +1,45 @@
 --
 -- this will test if current_setting and set_setting
--- provide a way to persist settings in the same session
--- inside of a CREATE INDEX statement
+-- provide a way to persist data when used in a function
+-- that is used to build an expression index using CREATE INDEX
+--
 -- (the function that's going to call current_setting and
 --  set_setting will be the same one that's going to be used
 --  to create an expression index)
 --
--- the main versions to be tested here will be 9.6.2 and 9.6.8
+-- the main versions to be tested here are 9.6.2 and 9.6.8
 -- but also other versions as well.
+--
+--
+
+
+CREATE OR REPLACE FUNCTION public.isempty(p_string character varying) RETURNS boolean
+LANGUAGE sql IMMUTABLE
+AS $$
+SELECT input IS NULL OR input = '';
+$$
+
+
+
+CREATE OR REPLACE FUNCTION public.test1(p_string character varying) RETURNS character varying
+LANGUAGE plpgsql IMMUTABLE
+AS $$
+
+persist TEXT;
+DECLARE
+
+BEGIN
+
+    BEGIN
+        SELECT current_setting('prefix.persist') INTO STRICT persist;
+        IF isempty('persist')
+            RAISE EXCEPTION 'persist session variable not set';
+        END IF;
+    EXCEPTION
+        PERFORM set_config('prefix.persist', 'value',false);
+    END;
+
+END;
+$$;
 
 
